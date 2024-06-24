2624bf1dabea29aa075e7ab8bd76a02c1a2df585
Farmer <mjombapadre@gmail.com>
2024-06-24 23:01:35 +0300

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char *letters = "abcdefghijklmnopqrstuvwxyz";
+    char *letters = "abcdefghijklmnopqrstuvwxyz";  // Pointer to the alphabet string
     int i;
 
     for (i = 0; letters[i] != '\0'; i++)
     {
-        putchar(letters[i]);
+        putchar(letters[i]);  // Print each character of the alphabet
     }
-    putchar('\n');
-    return (0);
+    
+    putchar('\n');  // Print a newline character at the end
+
+    return 0;
 }
 
