83611dd303b2d09417811b9a02b785515d1ee861
Farmer <mjombapadre@gmail.com>
2024-06-27 02:30:41 +0300

    commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,15 +10,15 @@
 int print_sign(int n)
 {
     if (n > 0)
-    {
-        return ('+');
+    {	_putchar ('-');
+        return (1);
     }
     else if (n < 0)
-    {
-        return ('-');
+    {	_putchar ('-');
+        return (-1);
     }
     else
-    {
+    {	_putchar (0);
         return (0);
     }
 }
