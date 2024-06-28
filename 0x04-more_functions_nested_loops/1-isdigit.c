f743c6d23cea335a2336d595eb481769b6f78f7d
Farmer <mjombapadre@gmail.com>
2024-06-29 02:19:27 +0300

    commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,6 +9,9 @@
  */
 int _isdigit(int c)
 {
-return (c >= '0' && c <= '9');
+    if (c >= '0' && c <= '9')
+    {
+        return (1);
+    }
+    return (0);
 }
-
