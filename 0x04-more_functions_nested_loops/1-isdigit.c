e91b35f5ceff44f93e107a79af4edc430ca4a77a
Farmer <mjombapadre@gmail.com>
2024-06-29 02:03:35 +0300

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * _isdigit - checks if a character is a digit
+ * @c: the character to check
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        if (c == i)
+        {
+            return (1);
+        }
+    }
+    return (0);
+}
+
