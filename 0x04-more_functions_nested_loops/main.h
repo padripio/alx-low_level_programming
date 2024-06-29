1d4723ddff8aeaf1a7826fcc5e3479ced5e1eef8
Farmer <mjombapadre@gmail.com>
2024-06-29 03:15:53 +0300

    commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,12 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
-#include <stdio.h>
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
-int mul(int a ,int b);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 
-#endif /* MAIN_H */
+#endif
