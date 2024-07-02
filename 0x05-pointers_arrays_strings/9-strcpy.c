2addc71dddaed394a8a47a59045f5d262417af57
Farmer <mjombapadre@gmail.com>
2024-07-02 03:11:07 +0300

    commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,8 +13,7 @@ char *_strcpy(char *dest, char *src)
 {
 	int i, len;
 
-	len = 0;
-	
+	len = 0;	
 	while (src[len] != '\0')
 	{
 		len++;
@@ -24,6 +23,5 @@ char *_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
-	
 	return (dest);
 }
